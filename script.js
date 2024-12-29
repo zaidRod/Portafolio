d8fbfddbecc05f1e74732d8c0a126ae2c527c228
zaid <zaidrod08@gmail.com>
2024-12-29 16:08:09 +0100

    iconos cambiados carga del curriculum

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 let menuVisible = false;
 // Funcion que oculta y muestra el menu
-function mostrarOcultarMenu(){
-    if(menuVisible){
-        document.getElementById("nav").classList="";
+function mostrarOcultarMenu() {
+    if (menuVisible) {
+        document.getElementById("nav").classList = "";
         menuVisible = false;
-    }else{
-        document.getElementById("nav").classList="responsive";
-        menuVisible= true;
+    } else {
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
     }
 
 
 }
 
-function seleccionar(){
+function seleccionar() {
     // oculto el menu ynas que se selecciona una opción
 
-    document.getElementById("nav").classList="";
-    menuVisible= false;
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
 }
 
-// Función que aplica las animaciones a las habilidades
+function descargarCv() {
+    const enlace = document.createElement("a");
+    enlace.href = "img/Curriculum Z Prog Dic 2024.pdf"; // Ruta al archivo PDF
+    enlace.download = "Zaid_Rodriguez_CV.pdf";          // Nombre del archivo al descargar
+    enlace.style.display = 'none';                       // Asegura que el enlace no se vea
+    document.body.appendChild(enlace);                   // Agrega el enlace al DOM
+    enlace.click();                                     // Simula un clic para descargar
+  
+  }
