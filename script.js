6007e75fd350f7386e66879c6352e5147ba35630
zaid <zaidrod08@gmail.com>
2025-01-28 13:17:56 +0100

    Imagenes de proyecto agregadas, boton de copiar,

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+//Llamado a la función para copiar el correo
+window.onload = function() {
+    copiarPorta();
+}
+
 let menuVisible = false;
 // Funcion que oculta y muestra el menu
 function mostrarOcultarMenu() {
@@ -12,8 +17,34 @@ function mostrarOcultarMenu() {
 
 }
 
+
+/* document.querySelectorAll(".modal-Contenedor img").forEach(el=>{
+    el.addEventListener("click", function(ev){  
+        ev.stopPropagation();
+        this.parentNode.classList.add("active");
+        console.log("click");
+    })
+}); */
+
+/* document.querySelectorAll(".modal-Contenedor img").forEach(el=>{
+    el.addEventListener("click", function(){
+        this.parentNode.remove("active");
+    })
+}); */
+
+
+function copiarPorta() {
+    document.getElementById("copiarPorta").addEventListener("click", function (event) {
+        //Evita que al ser un enlace se ejecute el comportamiento normal.
+        event.preventDefault();
+        let correo = "zaidrod08@gmail.com";
+        navigator.clipboard.writeText(correo).then(() => alert("¡Correo copiado!")).catch(error => alert("Error al copiar" + error));
+    })
+
+}
+
 function seleccionar() {
-    // oculto el menu ynas que se selecciona una opción
+    // oculto el menu y las que se selecciona una opción
 
     document.getElementById("nav").classList = "";
     menuVisible = false;
